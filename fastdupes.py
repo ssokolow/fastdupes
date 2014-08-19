6d6ef87c80115ecbbe93c6bcc7ef3b9c5a21f787
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2014-08-19 02:35:24 -0400

    Add a LICENSE file and header for the GPL 2.0
    (Note: My earliest commits probably go back further than 2009, but I don't
     think I was keeping records on this before then so I have no idea
     when my earliest commits were made.)

--- fastdupes.py
+++ fastdupes.py
@@ -82,6 +82,20 @@ more quickly than fdupes by using smarter algorithms.
 
 @newfield appname:Application Name
 
+Copyright (C) 2009-2014 Stephan Sokolow
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
 """
 
 
